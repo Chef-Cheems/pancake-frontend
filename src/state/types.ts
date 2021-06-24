889cf3a59a14a5e6329570527bb200faa023d1d8
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-06-24 09:21:34 +0100

    chore(lottery-v2): Strip out state involving future lotteries

--- src/state/types.ts
+++ src/state/types.ts
@@ -286,7 +286,6 @@ export interface LotteryState {
   currentLotteryId: string
   maxNumberTicketsPerBuyOrClaim: string
   currentRound: LotteryRound
-  nextRound?: LotteryRound
   pastLotteries?: PastLotteryRound[]
   userLotteryHistory?: UserLotteryHistory
 }
