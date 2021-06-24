529f400eb3a49785e1cc31b5125ffa82900720c0
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-06-24 17:15:03 +0100

    feat(lottery-v2): Lottery history and ticket fetch as part of same request

--- src/state/types.ts
+++ src/state/types.ts
@@ -398,6 +398,7 @@ export interface UserRound {
   claimed: boolean
   lotteryId: string
   totalTickets: string
+  tickets?: LotteryTicket[]
 }
 
 export type UserTicketsResponse = [ethers.BigNumber[], number[], boolean[], ethers.BigNumber]
