85cab92f43b7b0d5aa618467e23d2b7fd5dc688c
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-06-25 13:31:47 +0100

    chore(lottery-v2): Range of graph data var name changes. Decouple from SC reqs

--- src/state/types.ts
+++ src/state/types.ts
@@ -341,9 +341,9 @@ export interface VotingState {
   }
 }
 
-export interface LotteryRoundUserData {
+export interface LotteryRoundUserTickets {
+  isLoading?: boolean
   tickets?: LotteryTicket[]
-  isLoading: boolean
 }
 
 export interface LotteryRound {
@@ -362,18 +362,18 @@ export interface LotteryRound {
   cakePerBracket: string[]
   countWinnersPerBracket: string[]
   rewardsBreakdown: string[]
-  userData?: LotteryRoundUserData
+  userTickets?: LotteryRoundUserTickets
 }
 
 export interface LotteryState {
   currentLotteryId: string
   maxNumberTicketsPerBuyOrClaim: string
   currentRound: LotteryRound
-  pastLotteries?: PastLotteryRound[]
-  userLotteryData?: UserLotteryData
+  lotteriesData?: LotteryRoundGraphEntity[]
+  userLotteryData?: LotteryUserGraphEntity
 }
 
-export interface PastLotteryRound {
+export interface LotteryRoundGraphEntity {
   id: string
   totalUsers: string
   totalTickets: string
@@ -387,7 +387,7 @@ export interface PastLotteryRound {
   lastTicket: string
 }
 
-export interface UserLotteryData {
+export interface LotteryUserGraphEntity {
   account: string
   totalCake: string
   totalTickets: string
