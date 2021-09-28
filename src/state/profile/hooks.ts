2e82d45686b8e47ec7189fda299baa19a60ec1e0
Chef Cheems <chefcheems@protonmail.com>
2021-09-28 13:28:04 +0300

    chore: Reuse useGetProfileAvatar hook and add loading flags

--- src/state/profile/hooks.ts
+++ src/state/profile/hooks.ts
@@ -1,11 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import { useWeb3React } from '@web3-react/core'
 import { useSelector } from 'react-redux'
 import { isAddress } from 'utils'
 import { useAppDispatch } from 'state'
-import { State, ProfileState } from '../types'
+import { State, ProfileState, ProfileAvatarFetchStatus } from '../types'
 import { fetchProfile, fetchProfileAvatar, fetchProfileUsername } from '.'
-import { getProfile } from './helpers'
 
 export const useFetchProfile = () => {
   const { account } = useWeb3React()
@@ -18,26 +17,6 @@ export const useFetchProfile = () => {
   }, [account, dispatch])
 }
 
-export const useProfileForAddress = (address: string) => {
-  const [profileState, setProfileState] = useState({ profile: null, isFetching: false })
-  useEffect(() => {
-    const fetchProfileForAddress = async () => {
-      setProfileState({ profile: null, isFetching: true })
-      try {
-        const profile = await getProfile(address)
-        setProfileState({ profile, isFetching: false })
-      } catch (error) {
-        console.error(`Failed to fetch profile for address ${address}`, error)
-        setProfileState({ profile: null, isFetching: false })
-      }
-    }
-    if (!profileState.isFetching && !profileState.profile) {
-      fetchProfileForAddress()
-    }
-  }, [profileState, address])
-  return profileState
-}
-
 export const useProfile = () => {
   const { isInitialized, isLoading, data, hasRegistered }: ProfileState = useSelector((state: State) => state.profile)
   return { profile: data, hasProfile: isInitialized && hasRegistered, isInitialized, isLoading }
@@ -45,20 +24,25 @@ export const useProfile = () => {
 
 export const useGetProfileAvatar = (account: string) => {
   const profileAvatar = useSelector((state: State) => state.profile.profileAvatars[account])
-  const { username, nft } = profileAvatar || {}
+  const { username, nft, hasRegistered, usernameFetchStatus, avatarFetchStatus } = profileAvatar || {}
   const dispatch = useAppDispatch()
 
   useEffect(() => {
     const address = isAddress(account)
 
-    if (!nft && address) {
+    if (!nft && avatarFetchStatus !== ProfileAvatarFetchStatus.FETCHED && address) {
       dispatch(fetchProfileAvatar(account))
     }
 
-    if (!username && nft && address) {
-      dispatch(fetchProfileUsername(account))
+    if (
+      !username &&
+      avatarFetchStatus === ProfileAvatarFetchStatus.FETCHED &&
+      usernameFetchStatus !== ProfileAvatarFetchStatus.FETCHED &&
+      address
+    ) {
+      dispatch(fetchProfileUsername({ account, hasRegistered }))
     }
-  }, [account, nft, username, dispatch])
+  }, [account, nft, username, hasRegistered, avatarFetchStatus, usernameFetchStatus, dispatch])
 
-  return { username, nft }
+  return { username, nft, usernameFetchStatus, avatarFetchStatus }
 }
