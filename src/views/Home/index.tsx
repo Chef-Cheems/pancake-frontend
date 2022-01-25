65ac30da279e7cdb97abc429f60c950fe3ed7d69
Chef Cheems <chefcheems@protonmail.com>
2022-01-26 00:24:58 +0200

    chore: Develop vercel thingy

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -20,6 +20,9 @@ import FarmAuctionsBanner from './components/Banners/FarmAuctionsBanner'
 
 const showBanner = false
 
+// eslint-disable-next-line no-console
+console.log('I am develop branch')
+
 const HomeBanner = ({ account }: { account: string }) => {
   if (!showBanner) {
     return null
